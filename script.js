1ccb57564ea3b974da1fb474c2168701f34711be
Haibini <32377193+Haibini@users.noreply.github.com>
2019-03-21 15:14:28 -0700

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,24 @@
+//Spell #1
+
+$("#spell1").click(function(){
+ $(".img2").toggle();
+});
+
+
+
+//Spell #2
+
+$("#spell2").click(function(){
+ $(".img3").slideToggle();
+});
+
+
+
+//Spell #3
+
+$("#spell3").click(function(){
+ $(".img4").hide();
+});
+
+
+
